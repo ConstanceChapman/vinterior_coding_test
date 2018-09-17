92f3ce381eb227007fe4d4dd9b0a034855c312ad
Constance Chapman <constancealicechapman224@gmail.com>
2018-09-17 15:50:29 +0100

    refactor promo1 code

--- lib/promotional_rules.rb
+++ lib/promotional_rules.rb
@@ -2,9 +2,7 @@ class PromotionalRules
   def promo1(total, items)
     number_of_chairs = items["001"]
     if number_of_chairs && number_of_chairs >= 2
-      new_total = total - (9.25 * number_of_chairs)
-      discount_total = new_total + (8.5 * number_of_chairs)
-      return discount_total
+      return total - (9.25 * number_of_chairs) + (8.5 * number_of_chairs)
     end
     return total
   end
