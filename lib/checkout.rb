a75cc8df694c0a44dcec8140202049a7b35f5040
Constance Chapman <constancealicechapman224@gmail.com>
2018-09-17 12:12:45 +0100

    total function

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,5 +1,10 @@
 class Checkout
   def initialize(promo_rules)
     @promo_rules = promo_rules
+    @items = {}
+  end
+
+  def total(prices)
+    prices.reduce(:+)
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -8,4 +8,10 @@ describe Checkout do
       expect(co.class).to eql(Checkout)
     end
   end
+
+  describe "initialize variables" do
+    it "total should be sum of prices" do
+      expect(co.total([1, 1, 1])).to eql(3)
+    end
+  end
 end
