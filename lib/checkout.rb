6f382598bfc3031cc741cb4ea801ad70353dd207
Constance Chapman <constancealicechapman224@gmail.com>
2018-09-17 12:27:13 +0100

    basic scan method

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,7 +1,13 @@
 class Checkout
+  attr_accessor :items
   def initialize(promo_rules)
     @promo_rules = promo_rules
     @items = {}
+  end
+
+  def scan(item)
+    @items[item[:code]] = item
+    return @items
   end
 
   def total(prices)
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -21,4 +21,10 @@ describe Checkout do
       expect(co.total([1, 1, 1])).to eql("Discount!")
     end
   end
+
+  describe "scan method" do
+    it "item should be added to @items hash" do
+      expect(co.scan({code: 001, name: "chair", price: 9.25})).to eql({ 001 => {code: 001, name: "chair", price: 9.25}})
+    end
+  end
 end
