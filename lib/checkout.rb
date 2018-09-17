47bd7b0028d889c8d5e8617cf344cd9f771ffa5f
Constance Chapman <constancealicechapman224@gmail.com>
2018-09-17 12:04:38 +0100

    initialize checkout class

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,3 +1,5 @@
 class Checkout
-
+  def initialize(promo_rules)
+    @promo_rules = promo_rules
+  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,4 +1,11 @@
 require "checkout"
 
 describe Checkout do
+  let(:co) { Checkout.new(true) }
+
+  describe "checkout class" do
+    it "co should be an instance of checkout" do
+      expect(co.class).to eql(Checkout)
+    end
+  end
 end
